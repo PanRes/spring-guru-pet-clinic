4374c607b00b4de4eb76404e530bee80bda5e690
Panagiotis Ressos <p.ressos@hotmail.com>
2019-04-24 14:43:29 +0300

    Initial commit

---
+++ src/main/java/gr/pr/udemy/guru/petclinic/SpringGuruPetClinicApplication.java
@@ -0,0 +1,13 @@
+package gr.pr.udemy.guru.petclinic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringGuruPetClinicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringGuruPetClinicApplication.class, args);
+	}
+
+}
