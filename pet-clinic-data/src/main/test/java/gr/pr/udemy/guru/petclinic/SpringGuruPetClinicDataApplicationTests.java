97aba238a94d67f680f9da8ae73cc924ab0ded22
PR <p.ressos@hotmail.com>
2019-04-27 12:30:29 +0300

    Fixed data module's test class

--- pet-clinic-data/src/main/test/java/gr/pr/udemy/guru/petclinic/SpringGuruPetClinicDataApplicationTests.java
+++ pet-clinic-data/src/main/test/java/gr/pr/udemy/guru/petclinic/SpringGuruPetClinicDataApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class SpringGuruPetClinicWebApplicationTests {
+public class SpringGuruPetClinicDataApplicationTests {
 
 	@Test
 	public void contextLoads() {
