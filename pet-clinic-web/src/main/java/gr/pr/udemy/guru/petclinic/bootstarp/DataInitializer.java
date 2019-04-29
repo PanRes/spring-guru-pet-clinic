8dcdda6c1931cf5a72b097d37e55027c419ee4b7
Panagiotis Ressos <p.ressos@hotmail.com>
2019-04-29 23:41:56 +0300

    Auto generation of id in maps. Closes #23

--- pet-clinic-web/src/main/java/gr/pr/udemy/guru/petclinic/bootstarp/DataInitializer.java
+++ pet-clinic-web/src/main/java/gr/pr/udemy/guru/petclinic/bootstarp/DataInitializer.java
@@ -23,14 +23,12 @@ public class DataInitializer implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		Owner owner1 = new Owner();
-		owner1.setId(1L);
 		owner1.setFirstName("Panagiotis");
 		owner1.setLastName("Ressos");
 
 		ownerService.save(owner1);
 
 		Owner owner2 = new Owner();
-		owner2.setId(2L);
 		owner2.setFirstName("Makis");
 		owner2.setLastName("Kotsovos");
 
@@ -39,14 +37,12 @@ public class DataInitializer implements CommandLineRunner {
 		System.out.println("Owners Initialized");
 
 		Vet vet1 = new Vet();
-		vet1.setId(1L);
 		vet1.setFirstName("Filippos");
 		vet1.setLastName("Georgantas");
 
 		vetService.save(vet1);
 
 		Vet vet2 = new Vet();
-		vet2.setId(2L);
 		vet2.setFirstName("Angel");
 		vet2.setLastName("Pearl");
 
