8a92adf5ed2e9e06f33ce9a64a6dc33ac1242a87
Panagiotis Ressos <p.ressos@hotmail.com>
2019-09-01 15:01:29 +0300

    Added specialty entity and added relationship with Vet. Closes #28

--- pet-clinic-data/src/main/java/gr/pr/udemy/guru/petclinic/entity/Vet.java
+++ pet-clinic-data/src/main/java/gr/pr/udemy/guru/petclinic/entity/Vet.java
@@ -2,5 +2,13 @@ package gr.pr.udemy.guru.petclinic.entity;
 
 public class Vet extends Person {
 
+	private Specialty specialty;
 
+	public Specialty getSpecialty() {
+		return specialty;
+	}
+
+	public void setSpecialty(Specialty specialty) {
+		this.specialty = specialty;
+	}
 }
