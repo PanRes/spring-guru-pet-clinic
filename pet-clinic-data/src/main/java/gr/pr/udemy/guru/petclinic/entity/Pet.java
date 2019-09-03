9850fd1f14fd3c6b8f43b0ac644b405e545fc734
Panagiotis Ressos <p.ressos@hotmail.com>
2019-09-03 15:16:19 +0300

    Converted Visit to JPA entity. Closes #48

--- pet-clinic-data/src/main/java/gr/pr/udemy/guru/petclinic/entity/Pet.java
+++ pet-clinic-data/src/main/java/gr/pr/udemy/guru/petclinic/entity/Pet.java
@@ -2,6 +2,8 @@ package gr.pr.udemy.guru.petclinic.entity;
 
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "pets")
@@ -20,6 +22,9 @@ public class Pet extends BaseEntity {
 
 	@Column(name = "birth_date")
 	private LocalDate birthDate;
+
+	@OneToMany(mappedBy = "pet", orphanRemoval = true, cascade = CascadeType.ALL)
+	private Set<Visit> visits = new HashSet<>();
 
 	public Pet() {
 	}
@@ -67,5 +72,13 @@ public class Pet extends BaseEntity {
 
 	public void setBirthDate(LocalDate birthDate) {
 		this.birthDate = birthDate;
+	}
+
+	public Set<Visit> getVisits() {
+		return visits;
+	}
+
+	public void setVisits(Set<Visit> visits) {
+		this.visits = visits;
 	}
 }
--- pet-clinic-data/src/main/java/gr/pr/udemy/guru/petclinic/entity/Visit.java
+++ pet-clinic-data/src/main/java/gr/pr/udemy/guru/petclinic/entity/Visit.java
@@ -1,11 +1,20 @@
 package gr.pr.udemy.guru.petclinic.entity;
 
+import javax.persistence.*;
 import java.time.LocalDate;
 
+@Entity
+@Table(name = "visit")
 public class Visit extends BaseEntity{
 
+	@Column(name = "date", nullable = false)
 	private LocalDate date;
+
+	@Column(name = "description")
 	private String description;
+
+	@ManyToOne(optional = false)
+	@JoinColumn(name = "pet_id")
 	private Pet pet;
 
 	public Visit() {
