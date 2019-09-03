2e004647acfa0e5260f593bce74e83df817a43a2
Panagiotis Ressos <p.ressos@hotmail.com>
2019-09-03 15:58:58 +0300

    Created Spring Data JPA Service for Specialty. Closes #45

---
+++ pet-clinic-data/src/main/java/gr/pr/udemy/guru/petclinic/service/springdatajpa/SpecialtySDJpaService.java
@@ -0,0 +1,56 @@
+package gr.pr.udemy.guru.petclinic.service.springdatajpa;
+
+import gr.pr.udemy.guru.petclinic.entity.Specialty;
+import gr.pr.udemy.guru.petclinic.repository.SpecialtyRepository;
+import gr.pr.udemy.guru.petclinic.service.SpecialtyService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@RequiredArgsConstructor
+public class SpecialtySDJpaService implements SpecialtyService {
+
+	private final SpecialtyRepository specialtyRepository;
+
+	@Override
+	public Specialty findById(Long id) {
+		return specialtyRepository.findById(id).orElse(null);
+	}
+
+	@Override
+	public Set<Specialty> findAll() {
+
+		Set<Specialty> specialties = new HashSet<>();
+
+		specialtyRepository.findAll().forEach(specialties::add);
+
+		return specialties;
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		specialtyRepository.deleteById(id);
+	}
+
+	@Override
+	public void delete(Specialty specialty) {
+		specialtyRepository.delete(specialty);
+	}
+
+	@Override
+	public Specialty save(Specialty specialty) {
+		return specialtyRepository.save(specialty);
+	}
+
+	@Override
+	public Set<Specialty> saveAll(Iterable<Specialty> specialties) {
+		Set<Specialty> specialtiesToReturn = new HashSet<>();
+
+		specialtyRepository.saveAll(specialties).forEach(specialtiesToReturn::add);
+
+		return specialtiesToReturn;
+	}
+}
