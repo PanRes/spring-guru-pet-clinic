7711e22266dba4b110280a12c2240148b38a0112
Panagiotis Ressos <p.ressos@hotmail.com>
2019-09-03 15:55:46 +0300

    Created Spring Data JPA Service for Vet. Closes #42

---
+++ pet-clinic-data/src/main/java/gr/pr/udemy/guru/petclinic/service/springdatajpa/VetSDJpaService.java
@@ -0,0 +1,56 @@
+package gr.pr.udemy.guru.petclinic.service.springdatajpa;
+
+import gr.pr.udemy.guru.petclinic.entity.Vet;
+import gr.pr.udemy.guru.petclinic.repository.VetRepository;
+import gr.pr.udemy.guru.petclinic.service.VetService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@RequiredArgsConstructor
+public class VetSDJpaService implements VetService {
+
+	private final VetRepository vetRepository;
+
+	@Override
+	public Vet findById(Long id) {
+		return vetRepository.findById(id).orElse(null);
+	}
+
+	@Override
+	public Set<Vet> findAll() {
+
+		Set<Vet> vets = new HashSet<>();
+
+		vetRepository.findAll().forEach(vets::add);
+
+		return vets;
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		vetRepository.deleteById(id);
+	}
+
+	@Override
+	public void delete(Vet vet) {
+		vetRepository.delete(vet);
+	}
+
+	@Override
+	public Vet save(Vet vet) {
+		return vetRepository.save(vet);
+	}
+
+	@Override
+	public Set<Vet> saveAll(Iterable<Vet> vets) {
+		Set<Vet> vetsToReturn = new HashSet<>();
+
+		vetRepository.saveAll(vets).forEach(vetsToReturn::add);
+
+		return vetsToReturn;
+	}
+}
