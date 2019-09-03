e11b3238e547e9cb592a6c1201cedf12293f4989
Panagiotis Ressos <p.ressos@hotmail.com>
2019-09-03 15:52:29 +0300

    Created Spring Data JPA Service for PetType. Closes #43

---
+++ pet-clinic-data/src/main/java/gr/pr/udemy/guru/petclinic/service/springdatajpa/map/PetTypeSDJpaService.java
@@ -0,0 +1,56 @@
+package gr.pr.udemy.guru.petclinic.service.springdatajpa.map;
+
+import gr.pr.udemy.guru.petclinic.entity.PetType;
+import gr.pr.udemy.guru.petclinic.repository.PetTypeRepository;
+import gr.pr.udemy.guru.petclinic.service.PetTypeService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+@RequiredArgsConstructor
+public class PetTypeSDJpaService implements PetTypeService {
+
+	private final PetTypeRepository petTypeRepository;
+
+	@Override
+	public PetType findById(Long id) {
+		return petTypeRepository.findById(id).orElse(null);
+	}
+
+	@Override
+	public Set<PetType> findAll() {
+
+		Set<PetType> petTypes = new HashSet<>();
+
+		petTypeRepository.findAll().forEach(petTypes::add);
+
+		return petTypes;
+	}
+
+	@Override
+	public void deleteById(Long id) {
+		petTypeRepository.deleteById(id);
+	}
+
+	@Override
+	public void delete(PetType pet) {
+		petTypeRepository.delete(pet);
+	}
+
+	@Override
+	public PetType save(PetType pet) {
+		return petTypeRepository.save(pet);
+	}
+
+	@Override
+	public Set<PetType> saveAll(Iterable<PetType> petTypes) {
+		Set<PetType> petsToReturn = new HashSet<>();
+
+		petTypeRepository.saveAll(petTypes).forEach(petsToReturn::add);
+
+		return petsToReturn;
+	}
+}
