3b0bff3df27a5432c3410cacce0cb4b56ca2b3a3
Panagiotis Ressos <p.ressos@hotmail.com>
2019-09-04 17:22:07 +0300

    Fixed builder for Owner Entity

--- pet-clinic-data/src/main/java/gr/pr/udemy/guru/petclinic/entity/Owner.java
+++ pet-clinic-data/src/main/java/gr/pr/udemy/guru/petclinic/entity/Owner.java
@@ -33,9 +33,17 @@ public class Owner extends Person {
 		this.telephone = telephone;
 	}
 
-	@Builder
 	public Owner(String firstName, String lastName, String address, String city, String telephone, Set<Pet> pets) {
 		super(firstName, lastName);
+		this.address = address;
+		this.city = city;
+		this.telephone = telephone;
+		this.pets = pets;
+	}
+
+	@Builder
+	public Owner(Long id, String firstName, String lastName, String address, String city, String telephone, Set<Pet> pets) {
+		super(id, firstName, lastName);
 		this.address = address;
 		this.city = city;
 		this.telephone = telephone;
